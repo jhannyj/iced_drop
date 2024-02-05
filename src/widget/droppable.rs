1b2012c645ed6c06b29c6963f4ece764012f1da7
brianch <brianch@users.noreply.github.com>
2024-02-05 16:29:30 -0300

    Fix the centering measure
    Thanks @jhannyj for pointing out what the issue was.

--- src/widget/droppable.rs
+++ src/widget/droppable.rs
@@ -211,8 +211,8 @@ where
                             state.action = Action::Drag(start, position);
                             // update the position of the overlay since the cursor was moved
                             if self.drag_center {
-                                state.overlay_bounds.x = position.x - layout.bounds().width / 2.0;
-                                state.overlay_bounds.y = position.y - layout.bounds().height / 2.0;
+                                state.overlay_bounds.x = position.x - state.overlay_bounds.width / 2.0;
+                                state.overlay_bounds.y = position.y - state.overlay_bounds.height / 2.0;
                             } else {
                                 state.overlay_bounds.x = state.widget_pos.x + position.x - start.x;
                                 state.overlay_bounds.y = state.widget_pos.y + position.y - start.y;
